91ec054152814e5f6bd0a8cadd896cb5c2cc6b44
Matt Pocock <mattpocockvoice@gmail.com>
2022-10-14 18:36:45 +0100

    Added type predicate hints

--- src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.problem.ts
+++ src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.problem.ts
@@ -3,6 +3,9 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 export const values = ["a", "b", undefined, "c", undefined];
 
+// How can you change this line to make the test pass?
+// Clue - check out the docs on type predicates:
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-guards-and-differentiating-types
 const filteredValues = values.filter((value) => Boolean(value));
 
 it("Should filter out the undefined values", () => {
--- src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.solution.1.ts
+++ src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.solution.1.ts
@@ -3,6 +3,8 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 export const values = ["a", "b", undefined, "c", undefined];
 
+// This solution is a little ugly! The much better one
+// is solution number 2!
 const filteredValues = values.filter((value) => Boolean(value)) as string[];
 
 it("Should filter out the undefined values", () => {
