f6520ba4cc1c1f9043ac7b883d925ea19f29c4b0
Matt Pocock <mattpocockvoice@gmail.com>
2023-01-10 12:08:59 +0000

    Added an exercise on reverse mapped types

---
+++ src/06-identity-functions/30.5-reverse-mapped-types.problem.ts
@@ -0,0 +1,20 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+export function makeEventHandlers(obj: unknown) {
+  return obj;
+}
+
+const obj = makeEventHandlers({
+  click: (name) => {
+    // ^?
+    console.log(name);
+
+    type test = Expect<Equal<typeof name, "click">>;
+  },
+  focus: (name) => {
+    // ^?
+    console.log(name);
+
+    type test = Expect<Equal<typeof name, "focus">>;
+  },
+});
---
+++ src/06-identity-functions/30.5-reverse-mapped-types.solution.ts
@@ -0,0 +1,22 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+export function makeEventHandlers<T>(obj: {
+  [K in keyof T]: (name: K) => void;
+}) {
+  return obj;
+}
+
+const obj = makeEventHandlers({
+  click: (name) => {
+    // ^?
+    console.log(name);
+
+    type test = Expect<Equal<typeof name, "click">>;
+  },
+  focus: (name) => {
+    // ^?
+    console.log(name);
+
+    type test = Expect<Equal<typeof name, "focus">>;
+  },
+});
