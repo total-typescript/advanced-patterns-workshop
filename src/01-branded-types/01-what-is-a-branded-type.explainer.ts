fd7ffaf5c80defebed29e691e4fdd369ed549a56
Matt Pocock <mattpocockvoice@gmail.com>
2023-01-23 09:38:28 +0000

    Explainer code

--- src/01-branded-types/01-what-is-a-branded-type.explainer.ts
+++ src/01-branded-types/01-what-is-a-branded-type.explainer.ts
@@ -3,13 +3,25 @@ import { Brand } from "../helpers/Brand";
 type Password = Brand<string, "Password">;
 type Email = Brand<string, "Email">;
 
+type UserObject = Brand<
+  {
+    id: string;
+    name: string;
+  },
+  "User"
+>;
+
+const user: UserObject = {
+  id: "awdawd",
+  name: "awdawdawd",
+} as UserObject;
+
+const verifyPassword = (password: Password) => {};
+
 const password = "1231423" as Password;
 
 const email = "mpocock@me.com" as Email;
 
 let passwordSlot: Password;
 
-// @ts-expect-error
-passwordSlot = email;
-
-passwordSlot = password;
+passwordSlot = "awdjhawdjhbawd" as Password;
