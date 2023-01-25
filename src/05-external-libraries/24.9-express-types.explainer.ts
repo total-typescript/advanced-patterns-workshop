f8a96c766ae1703888bedda98dca40b68c10d970
Matt Pocock <mattpocockvoice@gmail.com>
2023-01-25 14:07:00 +0000

    Explainer

--- src/05-external-libraries/24.9-express-types.explainer.ts
+++ src/05-external-libraries/24.9-express-types.explainer.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send("Hello");
-});
+// /user?id=124123
+const getUser: RequestHandler<
+  any,
+  {
+    name: string;
+  },
+  any,
+  {
+    id: string;
+  }
+> = (req, res) => {
+  req.query.id;
+};
+
+app.get("/user");
+
+app.post("/user", (req, res) => {});
 
 app.listen(3000);
