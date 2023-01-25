ac3feb6a740862fbf0659872c0b8af102867449d
Matt Pocock <mattpocockvoice@gmail.com>
2023-01-25 12:05:21 +0000

    Changed explainer

--- src/04-classes/19.3-builder-pattern-intro.explainer.ts
+++ src/04-classes/19.3-builder-pattern-intro.explainer.ts
@@ -2,14 +2,20 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 export class BuilderTuple<TList extends any[] = []> {
   list: TList;
+
   constructor() {
     this.list = [] as any;
   }
 
-  push<TItem extends string | number | boolean>(
-    item: TItem,
-  ): BuilderTuple<[...TList, TItem]> {
-    this.list.push(item);
+  push<TNum extends number>(num: TNum): BuilderTuple<[...TList, TNum]> {
+    this.list.push(num);
+
+    return this as any;
+  }
+
+  unshift<TNum extends number>(num: TNum): BuilderTuple<[TNum, ...TList]> {
+    this.list.unshift(num);
+
     return this as any;
   }
 }
@@ -17,12 +23,12 @@ export class BuilderTuple<TList extends any[] = []> {
 const builderBeforePush = new BuilderTuple();
 const listBeforePush = builderBeforePush.list;
 
-const builderAfterPush = builderBeforePush.push(1).push(2).push(3);
+const builderAfterPush = builderBeforePush.unshift(3).unshift(2).unshift(1);
 const listAfterPush = builderAfterPush.list;
 
 type tests = [
   Expect<Equal<typeof builderBeforePush, BuilderTuple<[]>>>,
   Expect<Equal<typeof listBeforePush, []>>,
   Expect<Equal<typeof builderAfterPush, BuilderTuple<[1, 2, 3]>>>,
-  Expect<Equal<typeof listAfterPush, [1, 2, 3]>>,
+  Expect<Equal<typeof listAfterPush, [1, 2, 3]>>
 ];
