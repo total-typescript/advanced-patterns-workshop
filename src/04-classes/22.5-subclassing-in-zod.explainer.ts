3f418fc5c31a46b8d52fd5626a4daeb213c09178
Matt Pocock <mattpocockvoice@gmail.com>
2023-02-14 09:19:08 +0000

    Added subclassing in zod text

--- src/04-classes/22.5-subclassing-in-zod.explainer.ts
+++ src/04-classes/22.5-subclassing-in-zod.explainer.ts
@@ -1,4 +1,15 @@
 // Colin from Zod explains how using subclasses can make generics
 // look nice in intellisense.
 
+type ZodString = {
+  min: () => any;
+  max: () => any;
+} & ZodType<string, {}>;
+
+type ZodType<T, ZodDef> = {
+  parse: (input: unknown) => T;
+};
+
+const str: ZodString = {} as any;
+
 export {};
