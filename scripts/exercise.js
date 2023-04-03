ec810db5b13f991b180f226c29de2b3d4d0b8a6b
Matt Pocock <mattpocockvoice@gmail.com>
2023-04-03 12:18:35 +0100

    Improved exercise script

--- scripts/exercise.js
+++ scripts/exercise.js
@@ -3,6 +3,29 @@ const fs = require("fs");
 const path = require("path");
 const chokidar = require("chokidar");
 const fg = require("fast-glob");
+const tsconfig = require("../tsconfig.json");
+
+const compilerOptions = Object.entries(tsconfig.compilerOptions)
+  .map(([key, value]) => {
+    if (typeof value === "boolean") {
+      if (value) {
+        return `--${key}`;
+      }
+      return "";
+    }
+
+    if (key === "paths") return "";
+
+    if (Array.isArray(value)) {
+      return `--${key} ${value.join(" ")}`;
+    }
+
+    return `--${key} ${JSON.stringify(value)}`;
+  })
+  .filter(Boolean)
+  .join(" ");
+
+const specialPaths = Object.keys(tsconfig.compilerOptions.paths || {});
 
 const srcPath = path.resolve(__dirname, "../src");
 
@@ -48,11 +71,20 @@ chokidar.watch(exerciseFile).on("all", (event, path) => {
         stdio: "inherit",
       });
     }
-    console.log("Checking types...");
-    execSync(`tsc "${exerciseFile}" --noEmit --strict --esModuleInterop`, {
-      stdio: "inherit",
-    });
-    console.log("Typecheck complete. You finished the exercise!");
+
+    if (specialPaths.some((p) => fileContents.includes(p))) {
+      console.log(
+        `Due to a TS limitation, type checking won't work from the CLI on this file.`,
+      );
+      console.log(`Instead, follow the red lines in your IDE!`);
+    } else {
+      console.log("Checking types...");
+      const cmd = `tsc "${exerciseFile}" ${compilerOptions}`;
+      execSync(cmd, {
+        stdio: "inherit",
+      });
+      console.log("Typecheck complete. You finished the exercise!");
+    }
   } catch (e) {
     console.log("Failed. Try again!");
   }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
